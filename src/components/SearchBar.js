b42e877cf819feaa2c2b3646a8e97af21c341d9b
RayHaha <g820324@gmail.com>
2018-12-12 14:11:38 -0800

    handling searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,34 @@
 import React from 'react';
 
 class SearchBar extends React.Component{
+    state = { term: '' };
+
+    // this time we use the way different from practice3
+    onInputChange = (event) => {
+        this.setState({ term: event.target.value });
+    }
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+
+        // TODO: make sure we call callback from parent submit
+    }
+
     render(){
-        return <div>SearchBar</div>;
+        return (
+            <div className="search-bar ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
+                        <label>Video Search</label>
+                        <input 
+                            type="text" 
+                            value={this.state.term} 
+                            onChange={this.onInputChange}
+                        />
+                    </div>
+                </form>
+            </div>
+        );
     }
 }
 
