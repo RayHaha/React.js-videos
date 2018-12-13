1f89aed0d1ffe2050fc87da10d4c5b14e938cf4f
RayHaha <g820324@gmail.com>
2018-12-13 15:21:51 -0800

    add VideoList.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
+import VideoList from './VideoList';
 
 class App extends React.Component{
-    onTermSubmit = (term) => {
-        youtube.get('/search', {
+    // initialize state
+    state = { videos: [] };
+
+    // the async function to search while entering the term
+    onTermSubmit = async (term) => {
+        const response = await youtube.get('/search', {
             params: {
                 q: term
             }
         });
+        // use state to store the detail of video
+        this.setState({ videos: response.data.items});
     }
 
     render(){
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
+                <VideoList videos={this.state.videos} />
             </div>
         );
     }
